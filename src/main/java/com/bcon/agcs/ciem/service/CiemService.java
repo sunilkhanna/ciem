f2a3dcee2b76634140af66408df415a04b0d3f38
sunilkhanna <er.sunilkhanna@gmail.com>
2025-02-05 13:57:41 +0530

    Delete src directory

--- src/main/java/com/bcon/agcs/ciem/service/CiemService.java
+++
@@ -1,9 +0,0 @@
-package com.bcon.agcs.ciem.service;
-
-import com.bcon.agcs.ciem.model.CiemModel;
-import com.bcon.agcs.ciem.utils.FetchTypeEnum;
-import com.bcon.agcs.ciem.utils.TargetEnum;
-
-public interface CiemService {
-    CiemModel getDataByUserName(TargetEnum targetName, FetchTypeEnum fetchType, String userName);
-}
--- src/main/java/com/bcon/agcs/ciem/utils/CiemConstant.java
+++
@@ -1,8 +0,0 @@
-package com.bcon.agcs.ciem.utils;
-
-public class CiemConstant {
-    public static final String SLASH = "/";
-    public static final String USERID = "__USERID__";
-    public static final String ROLEID = "__ROLEID__";
-    public static final String APPID = "__APPID__";
-}
